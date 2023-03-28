cded315526a2f61a3b8f93c23fd5e6c78b3b9a8e
soareslil <lilianectd@gmail.com>
2023-03-28 16:03:26 -0300

    adicionando icone de favorito

--- src/app/componentes/pensamentos/pensamento/pensamento.component.ts
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -4,27 +4,32 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-pensamento',
   templateUrl: './pensamento.component.html',
-  styleUrls: ['./pensamento.component.css']
+  styleUrls: ['./pensamento.component.css'],
 })
 export class PensamentoComponent implements OnInit {
-
   @Input() pensamento: Pensamento = {
     id: 0,
     conteudo: 'I love Angular',
     autoria: 'Nay',
-    modelo: 'modelo3'
-  }
+    modelo: 'modelo3',
+    favorito: false,
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   larguraPensamento(): string {
-    if(this.pensamento.conteudo.length >= 256) {
-      return 'pensamento-g'
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
     }
-    return 'pensamento-p'
+    return 'pensamento-p';
   }
 
+  mudarIconeFavorito(): string {
+    if (this.pensamento.favorito == false) {
+      return 'inativo';
+    }
+    return 'ativo';
+  }
 }
